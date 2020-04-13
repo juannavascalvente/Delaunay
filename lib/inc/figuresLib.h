04b3499326a8be35f15c38683a8e97c5bd87924c
Juan Navas <juan.navas.calvente@gmail.com>
2020-04-13 20:36:40 +0200

    Task_0005: first commit to create new lib.h files

--- lib/inc/figuresLib.h
+++ lib/inc/figuresLib.h
@@ -1,5 +1,5 @@
-#ifndef DELAUNAY_DELAUNAYLIB_H
-#define DELAUNAY_DELAUNAYLIB_H
+#ifndef DELAUNAY_FIGURESLIB_H
+#define DELAUNAY_FIGURESLIB_H
 
 
 /***********************************************************************************************************************
@@ -7,37 +7,11 @@
 ***********************************************************************************************************************/
 #include "defines.h"
 #include "Dcel.h"
-#include "Delaunay.h"
-#include "Voronoi.h"
 
 
 /***********************************************************************************************************************
 * API function declarations
 ***********************************************************************************************************************/
-/**
- * @fn              getStarTriangulation
- * @brief           Returns the dcel associated to a star triangulation
- *
- * @param vPoints   (IN) Set of points
- * @param dcel      (OUT) Star triangulation in DCEL format
- * @return          true if triangulation built successfully
- *                  false otherwise
- */
-bool getStarTriangulation(vector<TYPE> &vPoints, Dcel &dcel);
-
-
-/**
- * @fn              getDelaunay
- * @brief           Returns the dcel associated to a Delaunay triangulation
- *
- * @param vPoints   (IN) Triangulation points
- * @param dcel      (OUT) Delunay triangulation in DCEL format
- * @return          true if triangulation built successfully
- *                  false otherwise
- */
-bool getDelaunay(vector<TYPE> &vPoints, Dcel &dcel);
-
-
 /**
  * @fn              getDelaunay
  * @brief           Returns the dcel associated to a Delaunay triangulation
@@ -61,5 +35,4 @@ bool getVoronoi(vector<Point<TYPE>> &vPoints, Dcel &dcelOut);
  */
 bool getConvexHull(vector<Point<TYPE>> &vPointsIn, vector<Point<TYPE>> &vPointsOut);
 
-
-#endif //DELAUNAY_DELAUNAYLIB_H
+#endif //DELAUNAY_FIGURESLIB_H
--- lib/src/figuresLib.cpp
+++ lib/src/figuresLib.cpp
@@ -1,67 +1,14 @@
 /***********************************************************************************************************************
 * Includes
 ***********************************************************************************************************************/
-#include "delaunayLib.h"
-#include "StarTriangulation.h"
+#include "figuresLib.h"
+#include "Delaunay.h"
+#include "Voronoi.h"
 
 
 /***********************************************************************************************************************
 * API function definitions
 ***********************************************************************************************************************/
-bool getStarTriangulation(vector<Point<TYPE>> &vPoints, Dcel &dcelOut)
-{
-    bool isSuccess;       // Return value
-
-    try
-    {
-        // Run command
-        auto *triangulation = new StarTriangulation(vPoints);
-        isSuccess = triangulation->build();
-
-        // Update output
-        dcelOut = *triangulation->getRefDcel();
-
-        // Free resources
-        delete triangulation;
-    }
-    catch (std::bad_alloc& ba)
-    {
-        std::cerr << "bad_alloc caught: " << ba.what() << '\n';
-        isSuccess = false;
-    }
-
-    return isSuccess;
-}
-
-
-bool getDelaunay(vector<Point<TYPE>> &vPoints, Dcel &dcelOut)
-{
-    bool isSuccess;       // Return value
-
-    try
-    {
-        // Insert points into delaunay
-        auto *delaunay = new Delaunay(vPoints);
-
-        // Build Delaunay using incremental algorithm
-        isSuccess = delaunay->build();
-
-        // Update output
-        dcelOut = *delaunay->getRefDcel();
-
-        // Free resources
-        delete delaunay;
-    }
-    catch (std::bad_alloc& ba)
-    {
-        std::cerr << "bad_alloc caught: " << ba.what() << '\n';
-        isSuccess = false;
-    }
-
-    return isSuccess;
-}
-
-
 bool getVoronoi(vector<Point<TYPE>> &vPoints, Dcel &dcelOut)
 {
     bool isSuccess;       // Return value
